f1f47b3d6f3f57b24b790e769b8527fd09beb076
Eric Lee <lee.eric.03@gmail.com>
2024-01-31 19:21:39 -0700

    updating event listener actions, need to add type for task object

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,21 @@ const form = document.querySelector<HTMLFormElement>("#task-form")
 const input = document.querySelector<HTMLInputElement>("#task-input")
 const submitBtn = document.querySelector<HTMLButtonElement>("#task-submit")
 
+form?.addEventListener("submit", e => {
+  e.preventDefault() 
+
+  if(input?.value == "" || input?.value == null) return 
+
+  const listItem = {
+    id: uuidv4(),
+    title: input.value,
+    completed: false, 
+    createdAt: new Date().toDateString()
+  }
+
+  
+})
+
+function addListItem(task) {
+  
+}
