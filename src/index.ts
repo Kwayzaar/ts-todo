d5589ac29a88c18aefa828eb5fe07f65cc38160a
Eric Lee <lee.eric.03@gmail.com>
2024-02-02 13:59:59 -0700

    added event handler, created Task Type, created addListItem function to append item to task list

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,42 @@
 import { v4 as uuidv4 } from 'uuid';
 
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean; 
+  createdAt: string; 
+}
+
 const taskList = document.querySelector<HTMLUListElement>("#task-list")
 const form = document.querySelector<HTMLFormElement>("#task-form")
 const input = document.querySelector<HTMLInputElement>("#task-input")
 const submitBtn = document.querySelector<HTMLButtonElement>("#task-submit")
 
 form?.addEventListener("submit", e => {
-  e.preventDefault() 
+  let d = new Date()
+  const dow = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"]
+  e.preventDefault()
+  console.log(e)
 
   if(input?.value == "" || input?.value == null) return 
 
-  const listItem = {
+  const listItem: Task = {
     id: uuidv4(),
     title: input.value,
     completed: false, 
-    createdAt: new Date().toDateString()
+    createdAt: `${dow[d.getDate()]}, ${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`
   }
 
-  
+  console.log(listItem)
+  addListItem(listItem)
 })
 
-function addListItem(task) {
-  
+function addListItem(task: Task) {
+  const item = document.createElement("li") 
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  label.append(checkbox, task.title)
+  item.append(label)
+  taskList?.append(item)
 }
